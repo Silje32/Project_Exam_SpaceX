bafb9f7ed612c5eeff565dcd85f3c3ef671b99f6
Silje Heldal Monsen <Silje24@yahoo.com>
2020-05-04 22:39:20 +0200

    completed the validation of the form

--- javascript/form.js
+++ javascript/form.js
@@ -27,37 +27,98 @@ form.addEventListener("submit", validateForm);
 
 function validateForm() {
   event.preventDefault(); //always have this
+
+
+  // test name
+  const nameValue = name.value;
+
+  if (validateLength(nameValue, 1) === true) {
+     nameError.style.display = "none";
+     nameHasError = false;
+  } else {
+     nameError.style.display = "block";
+     nameHasError = true;
+  }
+
+  // test last name
+  const lastNameValue = lastName.value;
+
+  if (validateLength(lastNameValue, 3) === true) {
+     lastNameError.style.display = "none";
+     lastNameHasError = false;
+  } else {
+    lastNameError.style.display = "block";
+    lastNameHasError = true;
+  }
+
+  // test email
+  const emailValue = email.value;
+
+  if (validateEmail(emailValue)) {
+     emailError.style.display = "none";
+     emailHasError = false;
+  } else {
+    emailError.style.display = "block";
+    emailHasError = true;
+  }
+
+  // test phone
+  const phoneValue = phone.value;
+
+  if (validatePhone(phoneValue)) {
+     phoneError.style.display = "none";
+     phoneHasError = false;
+  } else {
+    phoneError.style.display = "block";
+    phoneHasError = true;
+  }
+
+  // test message
+  const messageValue = message.value;
+
+  if (validateLength(messageValue, 6) === true) {
+     messageError.style.display = "none";
+     messageHasError = false;
+  } else {
+     messageError.style.display = "block";
+     messageHasError = true;
+  }
+
+  //decide whether to display the submitted message
+  if (nameHasError === true) || lastNameHasError === true || emailHasError === true ||
+     phoneHasError === true || messageHasError === true) {
+     submitMessage.style.display = "none";
+  } else {
+     submitMessage.style.display = "block";
+  }
 }
 
-// test name
-const nameValue = name.value;
+function validateLength(value, lengthToCheck) {
+  const trimmedValue = value.trim();
 
-if (validateLength(nameValue, 1) === true) {
-  nameError.style.display = "none";
-  nameHasError = false;
-} else {
-  nameError.style.display = "block";
-  nameHasError = true;
+  if (trimmedValue.length >= lengthToCheck) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
-// test last name
-const lastNameValue = lastName.value;
+function validateEmail(emailValue) {
+  const regEx = /\S+@\S+\.\S+/;
 
-if (validateLength(lastNameValue, 3) === true) {
-  lastNameError.style.display = "none";
-  lastNameHasError = false;
-} else {
-  lastNameError.style.display = "block";
-  lastNameHasError = true;
+  if (regEx.test(emailValue)) {
+     return true;
+  } else {
+     return false;
+  }
 }
 
-// test email
-const lastNameValue = lastName.value;
+function validatePhone(phoneValue) {
+  const regEx = /^\d{3}\ \d{3}\ \d{4}$/;
 
-if (validateLength(lastNameValue, 3) === true) {
-  lastNameError.style.display = "none";
-  lastNameHasError = false;
-} else {
-  lastNameError.style.display = "block";
-  lastNameHasError = true;
+  if (regEx.test(phoneValue)) {
+     return true;
+  } else {
+     return false;
+  }
 }
